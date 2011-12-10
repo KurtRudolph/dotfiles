0674794f2b029374740b054960a3e98b7620d609
Kurt Rudolph <rudolph9@illinois.edu>
2011-12-09 22:23:17 -0600

    added dropbox stuff

---
+++ .configure/configure_dropbox_folders.sh
@@ -0,0 +1,5 @@
+HOME=$HOME/.dropbox-acs /Applications/Dropbox.app/Contents/MacOS/Dropbox &
+HOME=$HOME/.dropbox-kurt /Applications/Dropbox.app/Contents/MacOS/Dropbox &
+HOME=$HOME/.dropbox-muldowneyfound /Applications/Dropbox.app/Contents/MacOS/Dropbox &
+HOME=$HOME/.dropbox-rudyind /Applications/Dropbox.app/Contents/MacOS/Dropbox &
+HOME=$HOME/.dropbox-school /Applications/Dropbox.app/Contents/MacOS/Dropbox &
