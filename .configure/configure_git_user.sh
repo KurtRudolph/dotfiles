ed4f881c64548ffb2b978976eea1a6e63e3741c8
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-04-03 00:46:34 -0500

    updated email

--- .configure/configure_git_user.sh
+++ .configure/configure_git_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git config --global user.name "Kurt Robert Rudolph"
-git config --global user.email rudolph9@illinois.edu
+git config --global user.email rudolph.kurt@rudyindustries.com
