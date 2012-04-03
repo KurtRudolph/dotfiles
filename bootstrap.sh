fe9f95eba5f615ba775495563b75babbf6c951de
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-04-03 01:33:00 -0500

    added submodule update components

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
+git submodule init
 git submodule sync
 git submodule update
+git submodule foreach git checkout master && git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
