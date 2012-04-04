0e97e7bb5833bee53ed9c936cbe07e6f336ce2de
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-04-04 06:53:20 -0500

    added bootstrap.rb to the exclude list

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,6 @@ git submodule foreach git checkout master && git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude ".gitignore" --exclude ".gitsubmodules" --exclude ".configure" -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.rb" --exclude "bootstrap.sh" --exclude "README.md" --exclude ".gitignore" --exclude ".gitsubmodules" --exclude ".configure" -av . ~
 fi
 source "$HOME/.bash_profile"
